329b28d95cdadd35966c5f2dd872e819397a1c18
flightlessbird <nws32908@gmail.com>
2025-10-09 09:57:37 -0400

    added iep to search with firefox

--- parts/modules/home/linux/modules/App-Config/browser/firefox/search/config/academic.nix
+++ parts/modules/home/linux/modules/App-Config/browser/firefox/search/config/academic.nix
@@ -25,4 +25,22 @@
       {template = "https://en.wikipedia.org/wiki/{searchTerms}";}
     ];
   };
+  "Internet Encyclopedia of Philosophy" = {
+    definedAliases = ["@iep"];
+    icon = ./icons/internet-encyclopedia-of-philosophy.png;
+    urls = [
+      {
+        template = "https://cse.google.com/cse?cx=001101905209118093242%3Arsrjvdp2op4&ie=UTF-8&q={searchTerms}&sa=Search";
+      }
+    ];
+  };
+  "Stanford Encyclopedia of Philosophy" = {
+    definedAliases = [
+      "@sep"
+    ];
+    icon = ./icons/ph.png;
+    urls = [
+      {template = "https://plato.stanford.edu/search/searcher.py?query={searchTerms}";}
+    ];
+  };
 }
--- parts/modules/home/linux/modules/App-Config/browser/firefox/search/config/tools.nix
+++ parts/modules/home/linux/modules/App-Config/browser/firefox/search/config/tools.nix
@@ -17,13 +17,4 @@
       {template = "https://www.urbandictionary.com/define.php?term={searchTerms}";}
     ];
   };
-  "Stanford Encyclopedia of Philosophy" = {
-    definedAliases = [
-      "@ph"
-    ];
-    icon = ./icons/ph.png;
-    urls = [
-      {template = "https://plato.stanford.edu/search/searcher.py?query={searchTerms}";}
-    ];
-  };
 }
