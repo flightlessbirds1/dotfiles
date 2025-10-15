58092eb43a0d57564138089f274225889f09b10e
flightlessbird <you@example.com>
2025-10-15 18:33:19 -0400

    added plasma, gaming, and compiling specializations (Plasma is broken, just saving it), improved structure a bit, added secrets for proxy stuff (removed it because it didn't work, but just keeping the secrets)

--- parts/modules/nixos_modules/interface/fonts/default.nix
+++ parts/modules/nixos_modules/interface/fonts/default.nix
@@ -3,10 +3,7 @@
     fontDir.enable = true;
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
-      nerd-fonts.symbols-only
-      noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
     ];
   };
 }
--- parts/modules/nixos_modules/system-dependent/desktop/boot.nix
+++ parts/modules/nixos_modules/system-dependent/desktop/boot.nix
@@ -1,5 +1,9 @@
 _: {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+    consoleMode = "auto";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = [
     "i2c-dev"
