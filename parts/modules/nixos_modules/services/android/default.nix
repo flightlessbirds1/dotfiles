7e5a02ead7ec3dc5a1f3ab5476ed5db7c885f117
flightlessbird <you@example.com>
2025-10-30 14:24:57 -0400

    updated flake

--- parts/modules/nixos_modules/services/android/default.nix
+++ parts/modules/nixos_modules/services/android/default.nix
@@ -1,16 +1,10 @@
-{pkgs, ...}: {
+{
   programs = {
     adb.enable = true;
     droidcam.enable = true;
   };
   services.udev = {
     enable = true;
-    packages = builtins.attrValues {
-      inherit
-        (pkgs)
-        android-udev-rules
-        ;
-    };
     extraRules = ''
       SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", MODE="0666"
       SUBSYSTEM=="usb", ATTR{idVendor}=="1949", MODE="0666", GROUP="adbusers"
